6577d7593c01dfb4d97141e4d06c8cf468331ece
Sergey <ommmirage@gmail.com>
2019-11-22 18:18:54 +0400

    101. Accessing object names

--- Escape/Source/Escape/PositionReport.cpp
+++ Escape/Source/Escape/PositionReport.cpp
@@ -2,6 +2,7 @@
 
 
 #include "PositionReport.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -18,7 +19,8 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on Chair!"));
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report for %s!"), *ObjectName);
 
 }
 
